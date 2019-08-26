11688687696b1b5d8e3c2b825974eac1afb968cf
harlam357 <harlam357@gmail.com>
2019-08-25 22:21:04 -0500

    Documentation, string interpolation, tests (47/569) 92%

--- HFM.Client/FahClientMessage.cs
+++ HFM.Client/FahClientMessage.cs
@@ -21,6 +21,9 @@ namespace HFM.Client
       public const string LogUpdate = "log-update";
    }
 
+   /// <summary>
+   /// Uniquely identifies a Folding@Home client message by the message type and date received.
+   /// </summary>
    public struct FahClientMessageIdentifier : IEquatable<FahClientMessageIdentifier>
    {
       /// <summary>
@@ -33,23 +36,42 @@ namespace HFM.Client
       /// </summary>
       public DateTime Received { get; }
 
+      /// <summary>
+      /// Initializes a new <see cref="FahClientMessageIdentifier"/>.
+      /// </summary>
+      /// <param name="messageType">The message type string.</param>
+      /// <param name="received">The date and time the message was received.</param>
       public FahClientMessageIdentifier(string messageType, DateTime received)
       {
          MessageType = messageType;
          Received = received;
       }
 
+      /// <summary>
+      /// Indicates whether the current <see cref="FahClientMessageIdentifier"/> is equal to another <see cref="FahClientMessageIdentifier"/>.
+      /// </summary>
+      /// <param name="other">An object to compare with this object.</param>
+      /// <returns>true if the current <see cref="FahClientMessageIdentifier"/> is equal to the <paramref name="other" /> parameter; otherwise, false.</returns>
       public bool Equals(FahClientMessageIdentifier other)
       {
          return String.Equals(MessageType, other.MessageType) && Received.Equals(other.Received);
       }
 
+      /// <summary>
+      /// Indicates whether this instance and a specified object are equal.
+      /// </summary>
+      /// <param name="obj">The object to compare with the current instance.</param>
+      /// <returns>true if <paramref name="obj" /> and this instance are the same type and represent the same value; otherwise, false.</returns>
       public override bool Equals(object obj)
       {
          if (ReferenceEquals(null, obj)) return false;
          return obj is FahClientMessageIdentifier other && Equals(other);
       }
 
+      /// <summary>
+      /// Returns the hash code for this instance.
+      /// </summary>
+      /// <returns>A 32-bit signed integer that is the hash code for this instance.</returns>
       public override int GetHashCode()
       {
          unchecked
@@ -59,18 +81,17 @@ namespace HFM.Client
       }
 
       /// <summary>
-      /// Returns a <see cref="T:System.String"/> that represents the current <see cref="T:System.Object"/>.
+      /// Returns a <see cref="String"/> that represents the current <see cref="FahClientMessageIdentifier"/>.
       /// </summary>
-      /// <returns>
-      /// A <see cref="T:System.String"/> that represents the current <see cref="T:System.Object"/>.
-      /// </returns>
-      /// <filterpriority>2</filterpriority>
       public override string ToString()
       {
-         return String.Format(CultureInfo.CurrentCulture, "Message Type: {0} - Received at: {1}", MessageType, Received);
+         return $"Message Type: {MessageType} - Received at: {Received}";
       }
    }
 
+   /// <summary>
+   /// Folding@Home client message.
+   /// </summary>
    public class FahClientMessage
    {
       /// <summary>
@@ -83,6 +104,11 @@ namespace HFM.Client
       /// </summary>
       public string MessageText { get; }
 
+      /// <summary>
+      /// Initializes a new instance of the <see cref="FahClientMessage"/> class.
+      /// </summary>
+      /// <param name="identifier">The message identifier.</param>
+      /// <param name="messageText">The text value of the message.</param>
       public FahClientMessage(FahClientMessageIdentifier identifier, string messageText)
       {
          Identifier = identifier;
@@ -90,16 +116,12 @@ namespace HFM.Client
       }
 
       /// <summary>
-      /// Returns a <see cref="T:System.String"/> that represents the current <see cref="T:System.Object"/>.
+      /// Returns a <see cref="String"/> that represents the current <see cref="FahClientMessage"/>.
       /// </summary>
-      /// <returns>
-      /// A <see cref="T:System.String"/> that represents the current <see cref="T:System.Object"/>.
-      /// </returns>
-      /// <filterpriority>2</filterpriority>
       public override string ToString()
       {
          var sb = new StringBuilder();
-         sb.AppendLine(String.Format(CultureInfo.CurrentCulture, "{0} - Length: {1}", Identifier, MessageText.Length));
+         sb.AppendLine($"{Identifier} - Length: {MessageText.Length}");
          sb.AppendLine(MessageText);
          sb.AppendLine();
          return sb.ToString();
