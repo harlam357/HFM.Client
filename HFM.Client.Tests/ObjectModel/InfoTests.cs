47b85f5ff2917e5a1130310248f0d484081e22be
harlam357 <harlam357@gmail.com>
2019-08-26 00:48:21 -0500

    begin to integrate object model

---
+++ HFM.Client.Tests/ObjectModel/InfoTests.cs
@@ -0,0 +1,170 @@
+﻿
+using NUnit.Framework;
+
+namespace HFM.Client.ObjectModel
+{
+   [TestFixture]
+   public class InfoTests
+   {
+      private const string InfoText = @"[
+  [
+    ""Folding @home Client"",
+    [
+      ""Website"",
+      ""http://folding.stanford.edu/""
+    ],
+    [
+      ""Copyright"",
+      ""(c) 2009,2010 Stanford University""
+    ],
+    [
+      ""Author"",
+      ""Joseph Coffland <joseph@cauldrondevelopment.com>""
+    ],
+    [
+      ""Args"",
+      "" --lifeline 1232 --command-port=36330""
+    ],
+    [
+      ""Config"",
+      ""C:/Documents and Settings/user/Application Data/FAHClient/config.xml""
+    ]
+  ],
+  [
+    ""Build"",
+    [
+      ""Version"",
+      ""7.1.24""
+    ],
+    [
+      ""Date"",
+      ""Apr  6 2011""
+    ],
+    [
+      ""Time"",
+      ""21:37:58""
+    ],
+    [
+      ""SVN Rev"",
+      ""2908""
+    ],
+    [
+      ""Branch"",
+      ""fah/trunk/client""
+    ],
+    [
+      ""Compiler"",
+      ""Intel(R) C++ MSVC 1500 mode 1110""
+    ],
+    [
+      ""Options"",
+      ""/TP /nologo /EHa /wd4297 /wd4103 /wd1786 /Ox -arch:SSE2 /QaxSSE3,SSSE3,SSE4.1,SSE4.2 /Qrestrict /MT""
+    ],
+    [
+      ""Platform"",
+      ""win32 Vista""
+    ],
+    [
+      ""Bits"",
+      ""32""
+    ],
+    [
+      ""Mode"",
+      ""Release""
+    ]
+  ],
+  [
+    ""System"",
+    [
+      ""OS"",
+      ""Microsoft(R) Windows(R) XP Professional x64 Edition""
+    ],
+    [
+      ""CPU"",
+      ""Intel(R) Core(TM)2 Quad CPU    Q6600  @ 2.40GHz""
+    ],
+    [
+      ""CPU ID"",
+      ""GenuineIntel Family 6 Model 15 Stepping 11""
+    ],
+    [
+      ""CPUs"",
+      ""4""
+    ],
+    [
+      ""Memory"",
+      ""4.00GiB""
+    ],
+    [
+      ""Free Memory"",
+      ""3.10GiB""
+    ],
+    [
+      ""Threads"",
+      ""WINDOWS_THREADS""
+    ],
+    [
+      ""GPUs"",
+      ""1""
+    ],
+    [
+      ""GPU 0"",
+      ""ATI:2 Mobility Radeon HD 3600 Series""
+    ],
+    [
+      ""CUDA"",
+      ""Not detected""
+    ],
+    [
+      ""On Battery"",
+      ""false""
+    ],
+    [
+      ""UTC offset"",
+      ""-5""
+    ],
+    [
+      ""PID"",
+      ""3080""
+    ],
+    [
+      ""CWD"",
+      ""C:/Documents and Settings/user/Application Data/FAHClient""
+    ],
+    [
+      ""Win32 Service"",
+      ""true""
+    ]
+  ]
+]";
+
+      [Test]
+      public void Info_FromJson_Test()
+      {
+         var info = Info.FromJson(InfoText);
+         Assert.AreEqual("http://folding.stanford.edu/", info.Client.Website);
+         Assert.AreEqual("(c) 2009,2010 Stanford University", info.Client.Copyright);
+         Assert.AreEqual("Joseph Coffland <joseph@cauldrondevelopment.com>", info.Client.Author);
+         Assert.AreEqual("--lifeline 1232 --command-port=36330", info.Client.Args);
+         Assert.AreEqual("C:/Documents and Settings/user/Application Data/FAHClient/config.xml", info.Client.Config);
+         Assert.AreEqual("7.1.24", info.Build.Version);
+         Assert.AreEqual("Apr  6 2011", info.Build.Date);
+         Assert.AreEqual("21:37:58", info.Build.Time);
+         Assert.AreEqual(2908, info.Build.SvnRev);
+         Assert.AreEqual("fah/trunk/client", info.Build.Branch);
+         Assert.AreEqual("Intel(R) C++ MSVC 1500 mode 1110", info.Build.Compiler);
+         Assert.AreEqual("/TP /nologo /EHa /wd4297 /wd4103 /wd1786 /Ox -arch:SSE2 /QaxSSE3,SSSE3,SSE4.1,SSE4.2 /Qrestrict /MT", info.Build.Options);
+         Assert.AreEqual("win32 Vista", info.Build.Platform);
+         Assert.AreEqual(32, info.Build.Bits);
+         Assert.AreEqual("Release", info.Build.Mode);
+         Assert.AreEqual("Microsoft(R) Windows(R) XP Professional x64 Edition", info.System.OperatingSystem);
+         Assert.AreEqual("Intel(R) Core(TM)2 Quad CPU    Q6600  @ 2.40GHz", info.System.Cpu);
+         Assert.AreEqual("GenuineIntel Family 6 Model 15 Stepping 11", info.System.CpuId);
+         Assert.AreEqual(4, info.System.CpuCount);
+         Assert.AreEqual("4.00GiB", info.System.Memory);
+         Assert.AreEqual(4.0, info.System.MemoryValue);
+         Assert.AreEqual("3.10GiB", info.System.FreeMemory);
+         Assert.AreEqual(3.1, info.System.FreeMemoryValue);
+      }
+   }
+}
---
+++ HFM.Client/ObjectModel/Info.cs
@@ -0,0 +1,197 @@
+﻿
+using System;
+using System.Linq;
+
+using Newtonsoft.Json.Linq;
+
+//using HFM.Client.Converters;
+
+namespace HFM.Client.ObjectModel
+{
+   /// <summary>
+   /// Folding@Home client info message.
+   /// </summary>
+   public class Info
+   {
+      /// <summary>
+      /// Initializes a new instance of the <see cref="Info"/> class.
+      /// </summary>
+      public Info()
+      {
+         Client = new ClientInfo();
+         Build = new BuildInfo();
+         System = new SystemInfo();
+      }
+
+      /// <summary>
+      /// Folding@Home client information.
+      /// </summary>
+      public ClientInfo Client { get; }
+
+      /// <summary>
+      /// Folding@Home client build information.
+      /// </summary>
+      public BuildInfo Build { get; }
+
+      /// <summary>
+      /// Folding@Home client system information.
+      /// </summary>
+      public SystemInfo System { get; }
+
+      /// <summary>
+      /// Creates a new <see cref="Info"/> object from the given JSON message.
+      /// </summary>
+      public static Info FromJson(string message)
+      {
+         var result = new Info();
+         var array = JArray.Parse(message);
+
+         var client = array[0];
+         result.Client.Website = GetValue<string>(client, "Website");
+         result.Client.Copyright = GetValue<string>(client, "Copyright");
+         result.Client.Author = GetValue<string>(client, "Author");
+         result.Client.Args = GetValue<string>(client, "Args")?.Trim();
+         result.Client.Config = GetValue<string>(client, "Config");
+
+         var build = array[1];
+         result.Build.Version = GetValue<string>(build, "Version");
+         result.Build.Date = GetValue<string>(build, "Date");
+         result.Build.Time = GetValue<string>(build, "Time");
+         result.Build.SvnRev = GetValue<int>(build, "SVN Rev");
+         result.Build.Branch = GetValue<string>(build, "Branch");
+         result.Build.Compiler = GetValue<string>(build, "Compiler");
+         result.Build.Options = GetValue<string>(build, "Options");
+         result.Build.Platform = GetValue<string>(build, "Platform");
+         result.Build.Bits = GetValue<int>(build, "Bits");
+         result.Build.Mode = GetValue<string>(build, "Mode");
+
+         var system = array[2];
+         result.System.OperatingSystem = GetValue<string>(system, "OS");
+         result.System.Cpu = GetValue<string>(system, "CPU");
+         result.System.CpuId = GetValue<string>(system, "CPU ID");
+         result.System.CpuCount = GetValue<int>(system, "CPUs");
+         result.System.Memory = GetValue<string>(system, "Memory");
+         result.System.MemoryValue = ConvertToMemoryValue(result.System.Memory);
+         result.System.FreeMemory = GetValue<string>(system, "Free Memory");
+         result.System.FreeMemoryValue = ConvertToMemoryValue(result.System.FreeMemory);
+
+         return result;
+      }
+
+      private static T GetValue<T>(JToken token, string name)
+      {
+         var innerArray = token.FirstOrDefault(x => x.FirstOrDefault()?.Value<string>() == name);
+         var valueElement = innerArray?.ElementAtOrDefault(1);
+         return valueElement != null ? valueElement.Value<T>() : default(T);
+      }
+
+      private static double? ConvertToMemoryValue(string input)
+      {
+         double value;
+         // always returns value in gigabytes
+         int gigabyteIndex = input.IndexOf("GiB", StringComparison.Ordinal);
+         if (gigabyteIndex > 0)
+         {
+            if (Double.TryParse(input.Substring(0, gigabyteIndex), out value))
+            {
+               return value;
+            }
+         }
+         int megabyteIndex = input.IndexOf("MiB", StringComparison.Ordinal);
+         if (megabyteIndex > 0)
+         {
+            if (Double.TryParse(input.Substring(0, megabyteIndex), out value))
+            {
+               return value / 1024;
+            }
+         }
+         int kilobyteIndex = input.IndexOf("KiB", StringComparison.Ordinal);
+         if (kilobyteIndex > 0)
+         {
+            if (Double.TryParse(input.Substring(0, kilobyteIndex), out value))
+            {
+               return value / 1048576;
+            }
+         }
+         return null;
+      }
+   }
+
+   /// <summary>
+   /// Folding@Home client information.
+   /// </summary>
+   public class ClientInfo
+   {
+      public string Website { get; set; }
+      public string Copyright { get; set; }
+      public string Author { get; set; }
+      public string Args { get; set; }
+      public string Config { get; set; }
+   }
+
+   /// <summary>
+   /// Folding@Home client build information.
+   /// </summary>
+   public class BuildInfo
+   {
+      public string Version { get; set; }
+      public string Date { get; set; }
+      public string Time { get; set; }
+      public int SvnRev { get; set; }
+      public string Branch { get; set; }
+      public string Compiler { get; set; }
+      public string Options { get; set; }
+      public string Platform { get; set; }
+      public int Bits { get; set; }
+      public string Mode { get; set; }
+   }
+
+   /// <summary>
+   /// Folding@Home client system information.
+   /// </summary>
+   public class SystemInfo
+   {
+      public string OperatingSystem { get; set; }
+      public string Cpu { get; set; }
+      public string CpuId { get; set; }
+      public int CpuCount { get; set; }
+      public string Memory { get; set; }
+      public double? MemoryValue { get; set; }
+      public string FreeMemory { get; set; }
+      public double? FreeMemoryValue { get; set; }
+      public string ThreadType { get; set; }
+      public int GpuCount { get; set; }
+      public string GpuId0 { get; set; }
+      //[MessageProperty("GPU 0", typeof(GpuTypeConverter))]
+      public string Gpu0 { get; set; }
+      public string GpuId1 { get; set; }
+      //[MessageProperty("GPU 1", typeof(GpuTypeConverter))]
+      public string Gpu1 { get; set; }
+      public string GpuId2 { get; set; }
+      //[MessageProperty("GPU 2", typeof(GpuTypeConverter))]
+      public string Gpu2 { get; set; }
+      public string GpuId3 { get; set; }
+      //[MessageProperty("GPU 3", typeof(GpuTypeConverter))]
+      public string Gpu3 { get; set; }
+      public string GpuId4 { get; set; }
+      //[MessageProperty("GPU 4", typeof(GpuTypeConverter))]
+      public string Gpu4 { get; set; }
+      public string GpuId5 { get; set; }
+      //[MessageProperty("GPU 5", typeof(GpuTypeConverter))]
+      public string Gpu5 { get; set; }
+      public string GpuId6 { get; set; }
+      //[MessageProperty("GPU 6", typeof(GpuTypeConverter))]
+      public string Gpu6 { get; set; }
+      public string GpuId7 { get; set; }
+      //[MessageProperty("GPU 7", typeof(GpuTypeConverter))]
+      public string Gpu7 { get; set; }
+      public string Cuda { get; set; }
+      public string CudaDriver { get; set; }
+      public bool HasBattery { get; set; }
+      public bool OnBattery { get; set; }
+      public int UtcOffset { get; set; }
+      public int ProcessId { get; set; }
+      public string WorkingDirectory { get; set; }
+      public bool? Win32Service { get; set; }
+   }
+}
