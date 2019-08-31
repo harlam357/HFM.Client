9adfb3f2631a7b4bd1dbf3bf68c39165a40268d4
harlam357 <harlam357@gmail.com>
2019-08-30 23:33:05 -0500

    Implement SlotCollection

---
+++ HFM.Client.Tests/ObjectModel/SlotCollectionTests.cs
@@ -0,0 +1,32 @@
+﻿
+using NUnit.Framework;
+
+namespace HFM.Client.ObjectModel
+{
+   [TestFixture]
+   public class SlotCollectionTests
+   {
+      private const string SlotCollectionText = @"[
+   {
+    ""id"": ""00"",
+      ""status"": ""RUNNING"",
+      ""description"": ""smp:4"",
+      ""options"": {
+         ""pause-on-start"": ""true""
+      }
+   }
+]";
+
+      [Test]
+      public void SlotCollection_FromMessage_Test()
+      {
+         var slotCollection = SlotCollection.FromMessage(SlotCollectionText);
+         Assert.AreEqual(1, slotCollection.Count);
+         Assert.AreEqual(0, slotCollection[0].ID);
+         Assert.AreEqual("RUNNING", slotCollection[0].Status);
+         Assert.AreEqual(SlotStatus.Running, slotCollection[0].SlotStatus);
+         Assert.AreEqual("smp:4", slotCollection[0].Description);
+         //Assert.AreEqual(true, slotCollection[0].SlotOptions.PauseOnStart);
+      }
+   }
+}
---
+++ HFM.Client/ObjectModel/SlotCollection.cs
@@ -0,0 +1,101 @@
+﻿
+using System;
+using System.Collections.ObjectModel;
+
+using Newtonsoft.Json.Linq;
+
+namespace HFM.Client.ObjectModel
+{
+   public class SlotCollection : Collection<Slot>
+   {
+      public static SlotCollection FromMessage(string message)
+      {
+         var result = new SlotCollection();
+
+         var jArray = JArray.Parse(message);
+         foreach (var token in jArray)
+         {
+            if (!token.HasValues)
+            {
+               continue;
+            }
+
+            var slot = Slot.FromJsonArrayElement(token.ToString());
+            result.Add(slot);
+         }
+         return result;
+      }
+   }
+
+   public class Slot
+   {
+      internal static Slot FromJsonArrayElement(string element)
+      {
+         var result = new Slot();
+
+         var obj = JObject.Parse(element);
+         result.ID = GetValue<int?>(obj, "id");
+         result.Status = GetValue<string>(obj, "status");
+         result.SlotStatus = ConvertToSlotStatus(result.Status);
+         result.Description = GetValue<string>(obj, "description");
+         return result;
+      }
+
+      private static T GetValue<T>(JObject obj, params string[] names)
+      {
+         foreach (var name in names)
+         {
+            var token = obj[name];
+            if (token != null)
+            {
+               try
+               {
+                  return token.Value<T>();
+               }
+               catch (FormatException)
+               {
+                  // if the data changed in a way where the value can
+                  // no longer be converted to the target CLR type
+               }
+            }
+         }
+         return default(T);
+      }
+
+      private static SlotStatus ConvertToSlotStatus(string input)
+      {
+         switch (input)
+         {
+            case "PAUSED":
+               return SlotStatus.Paused;
+            case "RUNNING":
+               return SlotStatus.Running;
+            case "FINISHING":
+               return SlotStatus.Finishing;
+            case "READY":
+               return SlotStatus.Ready;
+            case "STOPPING":
+               return SlotStatus.Stopping;
+            case "FAILED":
+               return SlotStatus.Failed;
+            default:
+               return SlotStatus.Unknown;
+         }
+      }
+
+      /// <summary>
+      /// Initializes a new <see cref="Slot"/>.
+      /// </summary>
+      public Slot()
+      {
+         //SlotOptions = new SlotOptions();
+      }
+
+      public int? ID { get; set; }
+      public string Status { get; set; }
+      public SlotStatus SlotStatus { get; set; }
+      public string Description { get; set; }
+      //[MessageProperty("options")]
+      //public SlotOptions SlotOptions { get; set; }
+   }
+}
