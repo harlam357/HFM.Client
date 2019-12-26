6f0fba582513b92bb6111b6ee6b72645c4f4438c
harlam357 <harlam357@gmail.com>
2019-12-26 12:40:40 -0600

    Add LogUpdateObjectLoader

---
+++ HFM.Client/ObjectModel/Internal/LogUpdateObjectLoader.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.IO;
+using System.Text;
+
+using HFM.Client.Internal;
+
+namespace HFM.Client.ObjectModel.Internal
+{
+    internal class LogUpdateObjectLoader : ObjectLoader<LogUpdate>
+    {
+        public override LogUpdate Load(string json)
+        {
+            return LoadInternal(new LogUpdate { Value = new StringBuilder(json) });
+        }
+
+        public override LogUpdate Load(StringBuilder json)
+        {
+            var logUpdate = new LogUpdate { Value = new StringBuilder() };
+            json.CopyTo(logUpdate.Value);
+            return LoadInternal(logUpdate);
+        }
+
+        public override LogUpdate Load(TextReader textReader)
+        {
+            return LoadInternal(new LogUpdate { Value = new StringBuilder(textReader.ReadToEnd()) });
+        }
+
+        private static LogUpdate LoadInternal(LogUpdate logUpdate)
+        {
+            int startIndex = GetStartIndex(logUpdate.Value);
+            int length = GetLength(logUpdate.Value, startIndex);
+            SetEnvironmentNewLineCharacters(logUpdate.Value.SubstringBuilder(startIndex, length, true));
+            return logUpdate;
+        }
+
+        private static int GetStartIndex(StringBuilder value)
+        {
+            int startIndex = value.EndIndexOf("\"", false);
+            return startIndex != -1 ? startIndex : 0;
+        }
+
+        private static int GetLength(StringBuilder value, int startIndex)
+        {
+            return (value.EndsWith('\"') ? value.Length - 1 : value.Length) - startIndex;
+        }
+
+        private static void SetEnvironmentNewLineCharacters(StringBuilder value)
+        {
+            value.Replace("\n", Environment.NewLine).Replace("\\n", Environment.NewLine);
+        }
+    }
+}
--- HFM.Client/ObjectModel/LogUpdate.cs
+++ HFM.Client/ObjectModel/LogUpdate.cs
@@ -1,8 +1,7 @@
 ﻿
 using System;
+using System.IO;
 using System.Text;
-
-using HFM.Client.Internal;
 
 namespace HFM.Client.ObjectModel
 {
@@ -11,36 +10,21 @@ namespace HFM.Client.ObjectModel
         /// <summary>
         /// Folding@Home client log update message.
         /// </summary>
-        public StringBuilder Value { get; private set; }
+        public StringBuilder Value { get; set; }
 
         /// <summary>
-        /// Creates a new <see cref="LogUpdate"/> object from the given log message.
+        /// Creates a new <see cref="LogUpdate"/> object from a <see cref="String"/> that contains the log text.
         /// </summary>
-        public static LogUpdate Create(StringBuilder message)
-        {
-            var result = new LogUpdate { Value = new StringBuilder() };
-            message.CopyTo(result.Value);
-            int startIndex = GetStartIndex(result.Value);
-            int length = GetLength(result.Value, startIndex);
-            SetEnvironmentNewLineCharacters(result.Value.SubstringBuilder(startIndex, length, true));
-            return result;
-        }
+        public static LogUpdate Load(string text) => new Internal.LogUpdateObjectLoader().Load(text);
 
-        private static int GetStartIndex(StringBuilder value)
-        {
-            int startIndex = value.EndIndexOf("\"", false);
-            return startIndex != -1 ? startIndex : 0;
-        }
+        /// <summary>
+        /// Creates a new <see cref="LogUpdate"/> object from a <see cref="StringBuilder"/> that contains the log text.
+        /// </summary>
+        public static LogUpdate Load(StringBuilder text) => new Internal.LogUpdateObjectLoader().Load(text);
 
-        private static int GetLength(StringBuilder value, int startIndex)
-        {
-            return (value.EndsWith('\"') ? value.Length - 1 : value.Length) - startIndex;
-        }
-
-        private static void SetEnvironmentNewLineCharacters(StringBuilder value)
-        {
-            value.Replace("\n", Environment.NewLine);
-            value.Replace("\\n", Environment.NewLine);
-        }
+        /// <summary>
+        /// Creates a new <see cref="LogUpdate"/> object from a <see cref="TextReader"/> that contains the log text.
+        /// </summary>
+        public static LogUpdate Load(TextReader textReader) => new Internal.LogUpdateObjectLoader().Load(textReader);
     }
 }
