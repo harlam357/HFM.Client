b2c8f221a685fc016e1355d8884b3e864bae40c9
harlam357 <harlam357@gmail.com>
2020-01-22 22:34:14 -0600

    Move FahClientCommand to its own file

--- HFM.Client/FahClientTextCommand.cs
+++ HFM.Client/FahClientTextCommand.cs
@@ -7,32 +7,6 @@ using System.Threading.Tasks;
 namespace HFM.Client
 {
     /// <summary>
-    /// Provides the abstract base class for a Folding@Home client command statement.
-    /// </summary>
-    public abstract class FahClientCommand
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="FahClientCommand"/> class.
-        /// </summary>
-        protected FahClientCommand()
-        {
-
-        }
-
-        /// <summary>
-        /// Sends the command to the Folding@Home client.
-        /// </summary>
-        /// <returns>The number of bytes transmitted to the Folding@Home client.</returns>
-        public abstract int Execute();
-
-        /// <summary>
-        /// Asynchronously sends the command to the Folding@Home client.
-        /// </summary>
-        /// <returns>A task representing the asynchronous operation that can return the number of bytes transmitted to the Folding@Home client.</returns>
-        public abstract Task<int> ExecuteAsync();
-    }
-
-    /// <summary>
     /// Folding@Home client text command statement.
     /// </summary>
     public class FahClientTextCommand : FahClientCommand
