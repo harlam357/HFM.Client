dec7eac68746395cadb911479f345a87aa3d870b
harlam357 <harlam357@gmail.com>
2020-01-22 22:41:36 -0600

    Move FahClientReader to its own file.

--- HFM.Client/FahClientMessageReader.cs
+++ HFM.Client/FahClientMessageReader.cs
@@ -7,24 +7,6 @@ using System.Threading.Tasks;
 namespace HFM.Client
 {
     /// <summary>
-    /// Provides the abstract base class for a Folding@Home client reader.
-    /// </summary>
-    public abstract class FahClientReader
-    {
-        /// <summary>
-        /// Advances the reader to the next data received from the Folding@Home client.
-        /// </summary>
-        /// <returns>true if data was read; otherwise false.</returns>
-        public abstract bool Read();
-
-        /// <summary>
-        /// Asynchronously advances the reader to the next data received from the Folding@Home client.
-        /// </summary>
-        /// <returns>A task representing the asynchronous operation that can return true if data was read; otherwise false.</returns>
-        public abstract Task<bool> ReadAsync();
-    }
-
-    /// <summary>
     /// Folding@Home client message reader.
     /// </summary>
     public class FahClientMessageReader : FahClientReader
---
+++ HFM.Client/FahClientReader.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Threading.Tasks;
+
+namespace HFM.Client
+{
+    /// <summary>
+    /// Provides the abstract base class for a Folding@Home client reader.
+    /// </summary>
+    public abstract class FahClientReader
+    {
+        /// <summary>
+        /// Advances the reader to the next data received from the Folding@Home client.
+        /// </summary>
+        /// <returns>true if data was read; otherwise false.</returns>
+        public abstract bool Read();
+
+        /// <summary>
+        /// Asynchronously advances the reader to the next data received from the Folding@Home client.
+        /// </summary>
+        /// <returns>A task representing the asynchronous operation that can return true if data was read; otherwise false.</returns>
+        public abstract Task<bool> ReadAsync();
+    }
+}
