02b323b46d16e12eab85aa4acf8340057ba4f209
harlam357 <harlam357@gmail.com>
2021-05-02 14:03:31 -0500

    Version 0.4.2-alpha Add code to decode JSON encoded with hex.  Fix "Bad JSON escape sequence: \x" error.

--- HFM.Client/ObjectModel/Internal/ObjectLoader.cs
+++ HFM.Client/ObjectModel/Internal/ObjectLoader.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.IO;
 using System.Text;
 
@@ -19,7 +18,7 @@ namespace HFM.Client.ObjectModel.Internal
         {
             if (json is null) return null;
 
-            using (var textReader = new StringReader(json))
+            using (var textReader = new StringBuilderReader(DecodeJson(json)))
             {
                 return Load(textReader);
             }
@@ -32,7 +31,7 @@ namespace HFM.Client.ObjectModel.Internal
         {
             if (json is null) return null;
 
-            using (var textReader = new StringBuilderReader(json))
+            using (var textReader = new StringBuilderReader(DecodeJson(json)))
             {
                 return Load(textReader);
             }
@@ -89,6 +88,115 @@ namespace HFM.Client.ObjectModel.Internal
                 }
             }
             return default;
+        }
+
+        protected static StringBuilder DecodeJson(string s)
+        {
+            if (String.IsNullOrEmpty(s))
+            {
+                return new StringBuilder(0);
+            }
+            if (!s.Contains(@"\"))
+            {
+                return new StringBuilder(s);
+            }
+            return DecodeJson(new StringBuilder(s));
+        }
+
+        // https://github.com/JamesNK/Newtonsoft.Json/issues/980
+        protected static StringBuilder DecodeJson(StringBuilder s)
+        {
+            if (s is null || s.Length == 0)
+            {
+                return s;
+            }
+
+            var builder = new StringBuilder();
+            int num = s.Length;
+            int num2 = 0;
+
+            while (num2 < num)
+            {
+                var ch = s[num2];
+                if (ch != 0x5c)
+                {
+                    builder.Append(ch);
+                }
+                else if (num2 < (num - 5) && s[num2 + 1] == 0x75)
+                {
+                    int num3 = HexToInt(s[num2 + 2]);
+                    int num4 = HexToInt(s[num2 + 3]);
+                    int num5 = HexToInt(s[num2 + 4]);
+                    int num6 = HexToInt(s[num2 + 5]);
+                    if (num3 < 0 || num4 < 0 | num5 < 0 || num6 < 0)
+                    {
+                        builder.Append(ch);
+                    }
+                    else
+                    {
+                        ch = (char)((((num3 << 12) | (num4 << 8)) | (num5 << 4)) | num6);
+                        num2 += 5;
+                        builder.Append(ch);
+                    }
+                }
+                else if (num2 < (num - 3) && s[num2 + 1] == 0x78)
+                {
+                    int num7 = HexToInt(s[num2 + 2]);
+                    int num8 = HexToInt(s[num2 + 3]);
+                    if (num7 < 0 || num8 < 0)
+                    {
+                        builder.Append(ch);
+                    }
+                    else
+                    {
+                        ch = (char)((num7 << 4) | num8);
+                        num2 += 3;
+                        builder.Append(ch);
+                    }
+                }
+                else
+                {
+                    if (num2 < (num - 1))
+                    {
+                        var ch2 = s[num2 + 1];
+                        if (ch2 == 0x5c)
+                        {
+                            builder.Append(@"\");
+                            num2 += 1;
+                        }
+                        else if (ch2 == 110)
+                        {
+                            builder.Append("\n");
+                            num2 += 1;
+                        }
+                        else if (ch2 == 0x74)
+                        {
+                            builder.Append("\t");
+                            num2 += 1;
+                        }
+                    }
+                    builder.Append(ch);
+                }
+                num2 += 1;
+            }
+            return builder;
+        }
+
+        private static int HexToInt(char h)
+        {
+            if (h < 0x30 || h > 0x39)
+            {
+                if (h < 0x61 || h > 0x66)
+                {
+                    if (h < 0x41 || h > 0x46)
+                    {
+                        return -1;
+                    }
+                    return h - 0x41 + 10;
+                }
+                return h - 0x61 + 10;
+            }
+            return h - 0x30;
         }
     }
 }
