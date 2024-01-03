5514786ddd299e232a9a90aa34156dfe0ed1575c
harlam357 <harlam357@gmail.com>
2024-01-02 21:00:04 -0600

    fix: TcpListener dispose warning for net8.0

--- HFM.Client.Tests/Sockets/LocalTcpListener.cs
+++ HFM.Client.Tests/Sockets/LocalTcpListener.cs
@@ -1,29 +1,30 @@
-﻿
-using System;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 
-namespace HFM.Client.Sockets
+namespace HFM.Client.Sockets;
+
+/// <summary>
+/// Simple TCP listener bound to localhost.
+/// </summary>
+internal class LocalTcpListener : IDisposable
 {
-    /// <summary>
-    /// Simple TCP listener bound to localhost.
-    /// </summary>
-    internal class LocalTcpListener : IDisposable
+    internal const string Host = "127.0.0.1";
+    internal const int Port = 13000;
+
+    private readonly TcpListener _tcpListener;
+
+    internal LocalTcpListener()
     {
-        internal const string Host = "127.0.0.1";
-        internal const int Port = 13000;
+        _tcpListener = new(IPAddress.Parse(Host), Port);
+        _tcpListener.Start();
+    }
 
-        private readonly TcpListener _tcpListener;
-
-        internal LocalTcpListener()
-        {
-            _tcpListener = new TcpListener(IPAddress.Parse(Host), Port);
-            _tcpListener.Start();
-        }
-
-        void IDisposable.Dispose()
-        {
-            _tcpListener.Stop();
-        }
+    void IDisposable.Dispose()
+    {
+#if NET8_0_OR_GREATER
+        _tcpListener.Dispose();
+#else
+        _tcpListener.Stop();
+#endif
     }
 }
