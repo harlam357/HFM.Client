50330026e298790e60820d05effc23b1683129de
harlam357 <harlam357@gmail.com>
2024-01-06 20:29:26 -0600

    refactor: add string parsing extensions

---
+++ HFM.Client/Internal/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace HFM.Client.Internal;
+
+internal static class StringExtensions
+{
+    internal static int? ToNullableInt32(this string value) =>
+        Int32.TryParse(value, out var result) ? result : null;
+
+    internal static double? ToNullableDouble(this string value) =>
+        Double.TryParse(value, out var result) ? result : null;
+
+    internal static bool? ToNullableBoolean(this string value) =>
+        Boolean.TryParse(value, out var result) ? result : null;
+}
